660dfee7e9998cd77fa747e443430c1968db8c60
Damien Burke <pair+damien@ministryofvelocity.com>
2016-03-23 00:14:41 -0700

    Add new attributes to conference presenter
    and add ability to handle some nil attributes

--- app/presenters/conference_presenter.rb
+++ app/presenters/conference_presenter.rb
@@ -1,7 +1,6 @@
 class ConferencePresenter < Struct.new(:instance)
   extend Forwardable
-  def_delegators :instance, :logo_url, :name, :website_url, :location,
-    :description, :tweets
+  def_delegators :instance, :logo_url, :name, :tweets, :hashtag, :code_of_conduct_url
 
   def twitter_name
     "@#{instance.twitter_handle}"
@@ -9,5 +8,68 @@ class ConferencePresenter < Struct.new(:instance)
 
   def twitter_url
     "https://twitter.com/#{instance.twitter_handle}"
+  end
+
+  def cfp_deadline
+    format_date instance.cfp_deadline
+  end
+
+  def speaker_notification_deadline
+    format_date instance.speaker_notification_deadline
+  end
+
+  def begin_date
+    format_date instance.begin_date
+  end
+
+  def end_date
+    format_date instance.end_date
+  end
+
+  def has_diversity_scholarships
+    instance.has_diversity_scholarships ? "Yes" : "No"
+  end
+
+  def has_childcare
+    instance.has_childcare ? "Yes" : "No"
+  end
+
+  def has_honorariums
+    instance.has_honorariums ? "Yes" : "No"
+  end
+
+  def has_lodging_funding
+    instance.has_lodging_funding ? "Yes" : "No"
+  end
+
+  def has_travel_funding
+    instance.has_travel_funding ? "Yes" : "No"
+  end
+
+  def location
+    instance.location || 'unknown'
+  end
+
+  def cfp_url
+    instance.cfp_url || 'none'
+  end
+
+
+  def website_url
+    instance.website_url || 'none'
+  end
+
+  def description
+    instance.description || 'none'
+  end
+
+  private
+
+  def format_date(date)
+    if date
+      date.strftime("%B %-d, %Y")
+    else
+      'unknown'
+    end
   end
 end
--- spec/presenters/conference_presenter_spec.rb
+++ spec/presenters/conference_presenter_spec.rb
@@ -6,21 +6,117 @@ RSpec.describe ConferencePresenter do
       logo_url: 'http://example.com/logo.png',
       name: 'Nameconf',
       twitter_handle: 'nameconf',
+      hashtag: '#nameconf',
       location: 'Dystopia',
       website_url: 'http://example.com',
-      description: 'A conference on names.'
+      description: 'A conference on names.',
+      cfp_deadline: DateTime.parse('April 3, 1980'),
+      cfp_url: 'https://example.com/cfps.aspx',
+      speaker_notification_deadline: DateTime.parse('April 4, 1980'),
+      begin_date: DateTime.parse('March 15, 1981'),
+      end_date: DateTime.parse('March 16, 1981'),
+      code_of_conduct_url: 'https://example.com/coc',
+      has_childcare: childcare,
+      has_diversity_scholarships: diversity_scholarships,
+      has_honorariums: honorariums,
+      has_travel_funding: travel_funding,
+      has_lodging_funding: lodging_funding,
     )
   end
   let!(:tweet) { conference.tweets.new(twitter_id: 'ham') }
+  let(:childcare) { nil }
+  let(:diversity_scholarships) { nil }
+  let(:honorariums) { nil }
+  let(:travel_funding) { nil }
+  let(:lodging_funding) { nil }
 
   subject(:presenter) { ConferencePresenter.new(conference) }
 
   specify { expect(presenter.logo_url).to eq('http://example.com/logo.png') }
   specify { expect(presenter.name).to eq('Nameconf') }
+  specify { expect(presenter.hashtag).to eq('#nameconf') }
   specify { expect(presenter.website_url).to eq('http://example.com') }
   specify { expect(presenter.location).to eq('Dystopia') }
   specify { expect(presenter.description).to eq('A conference on names.') }
   specify { expect(presenter.twitter_name).to eq('@nameconf') }
   specify { expect(presenter.twitter_url).to eq('https://twitter.com/nameconf') }
   specify { expect(presenter.tweets).to eq([tweet]) }
+  specify { expect(presenter.cfp_deadline).to be_a String }
+  specify { expect(presenter.cfp_deadline).to eq('April 3, 1980') }
+  specify { expect(presenter.speaker_notification_deadline).to eq('April 4, 1980') }
+  specify { expect(presenter.cfp_url).to eq('https://example.com/cfps.aspx') }
+  specify { expect(presenter.begin_date).to eq('March 15, 1981') }
+  specify { expect(presenter.end_date).to eq('March 16, 1981') }
+  specify { expect(presenter.code_of_conduct_url).to eq('https://example.com/coc') }
+
+  context 'conference has travel funding' do
+    let(:travel_funding) { true }
+    specify { expect(presenter.has_travel_funding).to eq('Yes') }
+  end
+  context 'conference does not have travel funding' do
+    let(:travel_funding) { false }
+    specify { expect(presenter.has_travel_funding).to eq('No') }
+  end
+
+  context 'conference has lodging funding' do
+    let(:lodging_funding) { true }
+    specify { expect(presenter.has_lodging_funding).to eq('Yes') }
+  end
+  context 'conference does not have lodging funding' do
+    let(:lodging_funding) { false }
+    specify { expect(presenter.has_lodging_funding).to eq('No') }
+  end
+
+  context 'conference has honorariums' do
+    let(:honorariums) { true }
+    specify { expect(presenter.has_honorariums).to eq('Yes') }
+  end
+  context 'conference does not have honorariums' do
+    let(:honorariums) { false }
+    specify { expect(presenter.has_honorariums).to eq('No') }
+  end
+
+  context 'conference has diversity scholarships' do
+    let(:diversity_scholarships) { true }
+    specify { expect(presenter.has_diversity_scholarships).to eq('Yes') }
+  end
+  context 'conference does not have diversity scholarships' do
+    let(:diversity_scholarships) { false }
+    specify { expect(presenter.has_diversity_scholarships).to eq('No') }
+  end
+
+  context 'conference has childcare' do
+    let(:childcare) { true }
+    specify { expect(presenter.has_childcare).to eq('Yes') }
+  end
+  context 'conference does not have childcare' do
+    let(:childcare) { false }
+    specify { expect(presenter.has_childcare).to eq('No') }
+  end
+
+  context 'Optional conference data is left blank' do
+    let(:conference) do
+      Conference.new(
+        logo_url: 'http://example.com/logo.png',
+        name: 'nameconf',
+        twitter_handle: 'nameconf',
+        location: nil,
+        website_url: nil,
+        description: nil,
+        cfp_deadline: nil,
+        cfp_url: nil,
+        begin_date: nil,
+        end_date: nil,
+        has_travel_funding: nil,
+      )
+    end
+
+    specify { expect(presenter.website_url).to eq('none') }
+    specify { expect(presenter.location).to eq('unknown') }
+    specify { expect(presenter.description).to eq('none') }
+    specify { expect(presenter.cfp_deadline).to eq('unknown') }
+    specify { expect(presenter.cfp_url).to eq('none') }
+    specify { expect(presenter.begin_date).to eq('unknown') }
+    specify { expect(presenter.end_date).to eq('unknown') }
+  end
 end
